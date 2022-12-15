29be8c3d7bb512564be3ef845ad189078ff65ace
JS <juancalderonh@gmail.com>
2022-12-15 13:04:10 -0500

    Commit Clase 20

--- src/componentes/CrearCuenta.js
+++ src/componentes/CrearCuenta.js
@@ -4,37 +4,37 @@ import swal from 'sweetalert';
 import crud from '../conexiones/crud';
 
 const CrearCuenta = () => {
-
+    
   const navigate = useNavigate();
 
-  const [usuario, setUsuario] = useState({
-    nombre: '',
-    email: '',
-    password: '',
-    confirmar: ''
+  const [usuario, setUsuario ] = useState({
+    nombre:'',
+    email:'',
+    password:'',
+    confirmar:''
   });
 
   const { nombre, email, password, confirmar } = usuario;
 
 
-  const onChange = (e) => {
-    setUsuario({
-      ...usuario,
-      [e.target.name]: e.target.value
-    })
+  const onChange = (e) =>{
+      setUsuario({
+        ...usuario,
+        [e.target.name]: e.target.value
+      })
   };
 
-  const crearCuenta = async () => {
+  const crearCuenta = async () =>{
     // los dos password deben ser iguales
-    if (password !== confirmar) {
+    if(password !== confirmar){
       console.log('diferntes');
       const mensaje = "Las contraseñas son diferentes.";
       swal({
-        title: 'Error',
+        title:'Error',
         text: mensaje,
         icon: 'error',
-        buttons: {
-          confirm: {
+        buttons:{
+          confirm:{
             text: 'OK',
             value: true,
             visible: true,
@@ -44,7 +44,7 @@ const CrearCuenta = () => {
         }
       });
 
-    } else {
+    }else{
       const data = {
         nombre: usuario.nombre,
         email: usuario.email,
@@ -54,30 +54,30 @@ const CrearCuenta = () => {
       const response = await crud.POST(`/api/usuarios`, data);
       const mensaje = response.msg;
       //console.log(mensaje);
-      if (mensaje === "El usuario ya existe") {
+      if(mensaje === "El usuario ya existe"){
         const mensaje = "El usuario ya existe";
-        swal({
-          title: 'Error',
-          text: mensaje,
-          icon: 'error',
-          buttons: {
-            confirm: {
-              text: 'OK',
-              value: true,
-              visible: true,
-              className: 'btn btn-danger',
-              closeModal: true
-            }
+      swal({
+        title:'Error',
+        text: mensaje,
+        icon: 'error',
+        buttons:{
+          confirm:{
+            text: 'OK',
+            value: true,
+            visible: true,
+            className: 'btn btn-danger',
+            closeModal: true
           }
-        });
-      } else {
+        }
+      });
+      }else{
         const mensaje = "El usuario fue creado correctamente";
         swal({
-          title: 'Información',
+          title:'Información',
           text: mensaje,
           icon: 'success',
-          buttons: {
-            confirm: {
+          buttons:{
+            confirm:{
               text: 'OK',
               value: true,
               visible: true,
@@ -88,105 +88,105 @@ const CrearCuenta = () => {
         });
 
         setUsuario({
-          nombre: '',
-          email: '',
-          password: '',
-          confirmar: ''
+          nombre:'',
+          email:'',
+          password:'',
+          confirmar:''
         })
         //redireccionar nuevamente a la pagina de login
-        navigate("/");
+        navigate("/login");
 
       };
     }
-
-
-
-
-
+    
+    
+    
+    
+    
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = (e) =>{
     e.preventDefault();
     crearCuenta();
   }
 
-
-
+  
+  
   return (
-    <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
+      <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
       <div className='md:w-2/3 lg:w-2/5'>
-        <h1 className="inline bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-          G12 Iniciar sesión Ecommerce
-        </h1>
-        <form
+      <h1 className="inline bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+         G12 Iniciar sesión Ecommerce 3
+       </h1>
+        <form 
           className="my-10 bg-white shadow rounded-lg p-10"
           onSubmit={onSubmit}
         >
-          <div className="my-5">
-
-            <label className="uppercase text-gray-600 block text-lx font-bold">Nombre</label>
-            <input
-              type="nombre"
-              id="nombre"
-              name="nombre"
-              placeholder="Ingrese su nombre"
-              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-              value={nombre}
-              onChange={onChange}
-            />
-            <label className="uppercase text-gray-600 block text-lx font-bold">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email de Registro"
-              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-              value={email}
-              onChange={onChange}
-            />
-
-
-
-            <label className="uppercase text-gray-600 block text-lx font-bold">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password de Registro"
-              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-              value={password}
-              onChange={onChange}
-            />
-
-            <label className="uppercase text-gray-600 block text-lx font-bold">Confirme su Password</label>
-            <input
-              type="confirmar"
-              id="confirmar"
-              name="confirmar"
-              placeholder="Confirmación Password"
-              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-              value={confirmar}
-              onChange={onChange}
-            />
-          </div>
-          <input
-            type="submit"
-            value="Registar Usuario"
-            className="bg-violet-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-violet-300 transition-colors"
-          />
-
-          <Link
-            className="block text-center my-5 "
-            to={"/"}
-          >
-            Regresar
-          </Link>
-        </form>
-
+         <div className="my-5">
+           
+         <label className="uppercase text-gray-600 block text-lx font-bold">Nombre</label>
+           <input 
+           type="nombre"
+           id="nombre"
+           name="nombre"
+           placeholder="Ingrese su nombre"
+           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+           value={nombre}
+           onChange={onChange}
+           />
+           <label className="uppercase text-gray-600 block text-lx font-bold">Email</label>
+           <input 
+           type="email"
+           id="email"
+           name="email"
+           placeholder="Email de Registro"
+           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+           value={email}
+           onChange={onChange}
+           />
+   
+          
+           
+           <label className="uppercase text-gray-600 block text-lx font-bold">Password</label>
+           <input 
+           type="password"
+           id="password"
+           name="password"
+           placeholder="Password de Registro"
+           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+           value={password}
+           onChange={onChange}
+           />
+           
+           <label className="uppercase text-gray-600 block text-lx font-bold">Confirme su Password</label>
+           <input 
+           type="password"
+           id="confirmar"
+           name="confirmar"
+           placeholder="Confirmación Password"
+           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+           value={confirmar}
+           onChange={onChange}
+           />
+         </div>
+         <input 
+             type="submit"
+             value="Registar Usuario"
+             className="bg-violet-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-violet-300 transition-colors"
+               />
+   
+         <Link
+           className="block text-center my-5 "
+         to={"/"}
+         >
+           Regresar
+         </Link>
+     </form>
+   
       </div>
-
-    </main>
-  )
+       
+      </main>
+    )
 
 }
 
